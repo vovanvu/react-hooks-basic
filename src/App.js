e5de4060bfa4860d493b0a8fa083d6f3b0a2f79b
Vu Vo <vanvu2912@gmail.com>
2022-02-27 21:56:21 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="app">
+        <h1>Welcome to React Hooks</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
